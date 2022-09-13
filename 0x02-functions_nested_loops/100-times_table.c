5bdea00f8b3344c3401b97c97bc805d71b701a98
Ronlin1 <ronlinx6@gmail.com>
2022-09-13 06:31:56 -0700

    push 100 again

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,8 +21,8 @@ void print_times_table(int n)
 				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
-					_putchar(' ');								
-					_putchar(' ');		
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
 				} else if (k >= 10 && k < 100)

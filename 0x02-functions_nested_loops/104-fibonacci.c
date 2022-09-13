414b1b3fc49a962d3bed05af7e83c60966b54dcd
Ronlin1 <ronlinx6@gmail.com>
2022-09-13 06:39:00 -0700

    re-wire 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,12 +21,11 @@ int main(void)
 		k = k + j;
 		j = k - j;
 	}
-
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
-	
+
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", k1 + (k2 / 1000000000));
@@ -36,6 +35,8 @@ int main(void)
 		k2 = k2 + j2;
 		j2 = k2 - j2;
 	}
+	
 	printf("\n");
+	
 	return (0);
 } 

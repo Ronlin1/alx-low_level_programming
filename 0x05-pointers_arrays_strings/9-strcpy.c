f54b84a78002543c07c513a93d41f30d9b1b5904
Ronlin1 <ronlinx6@gmail.com>
2022-09-19 14:17:01 -0700

    repush strcopy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,25 +1,30 @@
 #include "main.h"
 
 /**
- * _strcpy - function that copies the string pointed to by src
- * @dest: char pointer
- * @src: char pointer
- * Return: char.
+ *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int count = 0;
+	int len, i;
 
-	while (src[i] != 0)
+	len = 0;
+
+	while (src[len] != '\0')
 	{
-		count++;
-		i++;
+		len++;
 	}
-	for (i = 0; i <= count; i++)
+
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i + 1] = 0;
+	dest[i] = '\0';
+	
 	return (dest);
 }

8f07e094bf58136e2f3666be121329dba0627bab
Ronlin1 <ronlinx6@gmail.com>
2022-09-26 05:01:45 -0700

    repush 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,10 +11,10 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, f, flag;
+	
 	f = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
+	
+	for (i = 0; s[i] != '\0'; i++){
 		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
